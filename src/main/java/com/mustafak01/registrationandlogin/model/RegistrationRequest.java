d49626baf0d1b9327d21d7ce9bb9772bab7eb4df
Mustafa Köroğlu <mestefe119@gmail.com>
2022-04-18 09:06:42 +0300

    Create Models

---
+++ src/main/java/com/mustafak01/registrationandlogin/model/RegistrationRequest.java
@@ -0,0 +1,15 @@
+package com.mustafak01.registrationandlogin.model;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
+
+}
---
+++ src/main/java/com/mustafak01/registrationandlogin/model/UserRoleModel.java
@@ -0,0 +1,7 @@
+package com.mustafak01.registrationandlogin.model;
+
+public enum UserRoleModel {
+    USER,
+    ADMIN
+}
+
