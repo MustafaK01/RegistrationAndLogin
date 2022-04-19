014a87191e774adb9f772c1d2709d3295dc23bd0
Mustafa Köroğlu <mestefe119@gmail.com>
2022-04-19 10:32:24 +0300

    Update
    Instead of implementing UserDetails class in UserModel, I created a another class for UserDetails.
    
    And, with service annotation was giving an error like
    "required a bean of type 'java.lang.Long' that could not be found."
    When I removed the service annotation, the error went away.

--- src/main/java/com/mustafak01/registrationandlogin/model/UserModel.java
+++ src/main/java/com/mustafak01/registrationandlogin/model/UserModel.java
@@ -19,7 +19,7 @@ import java.util.Collections;
 @EqualsAndHashCode
 @NoArgsConstructor
 @Entity
-public class UserModel implements UserDetails {
+public class UserModel{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -28,59 +28,13 @@ public class UserModel implements UserDetails {
     private String userName;
     private String password;
     private String email;
-    private Boolean locked;//locked
-    private Boolean enabled;//enabled
 
-    @Enumerated(EnumType.STRING)
-    private UserRoleModel userRole;
 
-    public UserModel(String name,
-                     String userName,
-                     String password,
-                     UserRoleModel userRole,
-                     Boolean locked,
-                     Boolean enabled) {
+    public UserModel(Long id,String name, String userName,String password,String email) {
+        this.id = id;
         this.name = name;
         this.userName = userName;
         this.password = password;
-        this.userRole = userRole;
-        this.locked = locked;
-        this.enabled = enabled;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(userRole.name());
-        return Collections.singletonList(simpleGrantedAuthority);//it will return the value like [USER,ADMIN]
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return userName;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return !locked;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return enabled;
+        this.email = email;
     }
 }
