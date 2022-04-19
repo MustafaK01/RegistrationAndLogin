6b3016e8153fe1ac36ef89a702b184b2eeb49ac7
Mustafa Köroğlu <mestefe119@gmail.com>
2022-04-19 10:31:22 +0300

    Update UserRepository.java

--- src/main/java/com/mustafak01/registrationandlogin/repository/UserRepository.java
+++ src/main/java/com/mustafak01/registrationandlogin/repository/UserRepository.java
@@ -3,14 +3,11 @@ package com.mustafak01.registrationandlogin.repository;
 import com.mustafak01.registrationandlogin.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
-import java.util.Optional;
 
 @Repository
-@Transactional
 public interface UserRepository extends JpaRepository<UserModel,Long>{
-    Optional<UserModel> findByEmail(String email);
+    UserModel findByUserName(String userName);
+    UserModel findByEmail(String email);
 }
 
